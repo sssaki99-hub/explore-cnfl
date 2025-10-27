79dd785bf4691398acf6511eec6bc4f399088af5
sssaki99-hub <sssaki99@gmail.com>
2025-10-27 22:19:09 +0600

    Update AuthPage.tsx

--- pages/AuthPage.tsx
+++ pages/AuthPage.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { useAuth } from '../hooks/useAuth';
+import { useAuth } from '../hooks/useAuth.ts';
 import { useNavigate } from 'react-router-dom';
 
 const AuthPage: React.FC = () => {
